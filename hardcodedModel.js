49914827a5377d955af1bb6bf2ebe71176d55bf4
Kristian Woods <kris_woods@hotmail.co.uk>
2020-12-29 15:00:16 +0000

    create modal using the fetch api (not working 100% yet)

---
+++ hardcodedModel.js
@@ -0,0 +1,18 @@
+import Modal from './modal.js';
+
+
+export const hardCodedModal = new Modal({
+    modalType: 'ModalTwo',
+    title: 'My Modal',
+    bgImage: './assets/images/christmas-winter-composition-frame-made-600w-1224165394.jpg',
+    desc: 'big sale, blah, blah',
+    cta: [
+        {id: 'learnMore', text: 'LEARN MORE', link: '#', classes: ['learn-more'], eventHandler: () => {alert('Learn more')}},
+        {id: 'bookNow', text: 'BOOK NOW', link: '#', classes: ['learn-more'], eventHandler: () => {alert('Book Now')}},
+        {id: 'cancel', text: 'CANCEL', link: '#', classes: ['cancel'], eventHandler: () => {hardCodedModal.CloseModal();}}
+    ],
+    classes: ['modal-body', 'modalTwo'],
+    openOnPageLoad: true,
+    triggerModalCTA: '.openModal',
+    attachTo: '.page-787'
+});
