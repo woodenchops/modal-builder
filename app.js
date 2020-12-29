a0d6b43324d264fd2fa264d03da14cb4ad4aaeac
Kristian Woods <kris_woods@hotmail.co.uk>
2020-12-29 11:37:49 +0000

    add third cta

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const homepageModal = new Modal({
     desc: 'big sale, blah, blah',
     cta: [
         {id: 'learnMore', text: 'LEARN MORE', link: '#', classes: ['learn-more'], eventHandler: () => {alert('Learn more')}},
+        {id: 'bookNow', text: 'BOOK NOW', link: '#', classes: ['learn-more'], eventHandler: () => {alert('Book Now')}},
         {id: 'cancel', text: 'CANCEL', link: '#', classes: ['cancel'], eventHandler: () => {homepageModal.CloseModal();}}
     ],
-    classes: ['modal-body', 'modalOne'],
+    classes: ['modal-body', 'modalTwo'],
     openOnPageLoad: true,
     triggerModalCTA: '.openModal',
     attachTo: '.page-787'
 });
 
 
-console.log('continued code')
+console.log('continued code');
